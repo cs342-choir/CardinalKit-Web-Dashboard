8dae730fd38c72dc67e36d383b6a5cc0cb5ae909
Esteban <julianramos@alternova.co>
2021-05-10 17:13:02 -0500

    Add (firebase) Complete login logout flow with

--- cardinal-kit-web/src/router/index.js
+++ cardinal-kit-web/src/router/index.js
@@ -1,63 +1,122 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import store from '../store'
+import store from "../store";
 
-import Home from '../views/Home.vue'
-import Login from '@/views/auth/login'
-import SignUp from '@/views/auth/signUp'
+import Home from "../views/Home.vue";
+import Login from "@/views/auth/login";
+import SignUp from "@/views/auth/signUp";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
     meta: {
-      requiresAuth: true
-    }
+      // requiresAuth: true,
+    },
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/signup',
-    name: 'signup',
+    path: "/signup",
+    name: "signup",
     component: SignUp,
     meta: {
-      requiresAuth: true
-    }
-  }
-]
+      // requiresAuth: true,
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-  console.log()
-  // store.commit('general/setIsCharging', true)
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    if (!store.getters['auth/getUserIsLogged']) {
-      next({ name: 'Login' })
-    } else {
-      next() // go to wherever I'm going
-    }
+  console.log("to fuera de auth",to.name,"from",from.name)
+  if (process.env.VUE_APP_AUTH_MODE == "firebase") {
+    let { auth } = require("@/plugins/firebase/firebase");
+    let unsubscribe = auth.onAuthStateChanged(function(user){
+      console.log("to dentro de auth",to.name,"from",from.name)
+        if (user) {
+          if(to.name=="Login"){
+            console.log("nextHome")
+            next({name:'Home'})
+          }
+          else{
+            console.log("nextNormal1")
+            next()
+          }
+        } else {
+          
+          
+          if (to.matched.some((record) => record.meta.requiresAuth || to.name!="Login")) {
+            console.log("nextLogin")
+            next({name:'Login'})
+          }
+          else{
+            console.log("nextNormal2")
+            next()
+          }
+        }
+        unsubscribe()
+    });
+    // console.log(unsubscribe)
   } else {
-    next() // does not require auth, make sure to always call next()!
+
   }
-})
+});
 
+// router.beforeEach((to, from, next) => {
+//   console.log("Logged1",)
+//   if (localStorage.getItem("logged")) {
+//     store.commit('auth/isLogged',true)
+//     if (to.name == "Login") {
+//       next({ name: "Home" });
+//     } else {
+//       next();
+//     }
+//   } else {
+//     store.commit('auth/isLogged',false)
+//     if (to.matched.some((record) => record.meta.requiresAuth)) {
+//       next({ name: "Login" });
+//     } else {
+//       next();
+//     }
+//   }
 
-export default router
+// });
+
+// if (process.env.VUE_APP_AUTH_MODE == "firebase") {
+//   let { auth } = require("@/plugins/firebase/firebase");
+//   auth.onAuthStateChanged((user) => {
+//     console.log("callUserChange")
+//     let isLogged = user!=undefined
+//     let previousIsLogged = localStorage.getItem("logged")
+//     if(previousIsLogged!=isLogged){
+//       console.log("rerounteonauthChange")
+//       if(isLogged){
+//         console.log("pushHome")
+//         router.push({name:'Home'})
+//       }
+//       else{
+//         console.log("pushLogin")
+//         router.push({name:'Login'})
+//       }
+//     }
+//   });
+// }
+
+export default router;
