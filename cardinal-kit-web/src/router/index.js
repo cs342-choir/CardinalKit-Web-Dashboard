8081b2a8634cd4b402b08525b0a535f48e745e8f
Esteban <julianramos@alternova.co>
2021-05-10 14:21:31 -0500

    Add (Login) isLogged depend on auth method

--- cardinal-kit-web/src/router/index.js
+++ cardinal-kit-web/src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
+
 import Home from '../views/Home.vue'
 import Login from '@/views/auth/login'
 import SignUp from '@/views/auth/signUp'
@@ -7,7 +10,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/about',
@@ -25,14 +31,33 @@ const routes = [
   {
     path: '/signup',
     name: 'signup',
-    component: SignUp
+    component: SignUp,
+    meta: {
+      requiresAuth: true
+    }
   }
-
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  console.log()
+  // store.commit('general/setIsCharging', true)
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!store.getters['auth/getUserIsLogged']) {
+      next({ name: 'Login' })
+    } else {
+      next() // go to wherever I'm going
+    }
+  } else {
+    next() // does not require auth, make sure to always call next()!
+  }
+})
+
 
 export default router
---
+++ cardinal-kit-web/src/store/modules/authFirebase/getters.js
@@ -0,0 +1,3 @@
+export function getUserIsLogged(state) {
+    return state.isLogged
+  }
