088d53864d6451120cf804d0cf88a3a5ec481193
Esteban <julianramos@alternova.co>
2021-05-11 10:07:40 -0500

    Add (Request) base firebase rest

---
+++ cardinal-kit-web/src/Rest/api/index.js
@@ -0,0 +1,19 @@
+export const GET=async (path,payload)=>{
+
+}
+
+export const  POST= async (path,payload)=>{
+    
+}
+
+export const PATH= async (path,payload)=>{
+
+}
+
+export const  PUT= async ()=>{
+
+}
+
+export const DELETE= async (path,payload)=>{
+
+}
