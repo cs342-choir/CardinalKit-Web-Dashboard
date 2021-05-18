6ce33a429922cc52a52277ba6ef69b6eeefd6d9b
Esteban <julianramos@alternova.co>
2021-05-18 09:27:55 -0500

    Wip (Patients) patients graph and data

---
+++ cardinal-kit-web/src/components/patients/healthKit/activity/activityCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h4><b>{{ name }} </b></h4>
+      {{ date }}
+      <!-- Logo -->
+    </div>
+    <div class="card-body">
+      <slot name="card-body"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ActivityCard",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+  border-radius: 5px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+img {
+  border-radius: 5px 5px 0 0;
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
---
+++ cardinal-kit-web/src/components/patients/healthKit/category.vue
@@ -0,0 +1,57 @@
+<template>
+<div>     
+   <!-- <img :src="data.icon"/> -->
+    <button :onClick="handleSubmit">{{data.name}}</button>
+</div>
+
+</template>
+<script>
+    export default {
+        name: 'category',
+        props: {
+          data:{
+              type:Object,
+              required:true
+          },  
+          userId:{
+              type:String,
+              required:true
+          },
+          studyId:{
+              type:String,
+              required:true
+          }
+        },
+        data(){
+            return{
+                
+            }
+        },
+        methods: {
+            handleSubmit(){
+                this.$router.push(`/${this.data.path}/${this.studyId}/${this.userId}`)
+            }
+        },
+        computed:{
+            
+        },
+    };
+</script>
+
+
+<style>
+/* Style buttons */
+.btn {
+  background-color: DodgerBlue; /* Blue background */
+  border: none; /* Remove borders */
+  color: white; /* White text */
+  padding: 12px 16px; /* Some padding */
+  font-size: 16px; /* Set a font size */
+  cursor: pointer; /* Mouse pointer on hover */
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: RoyalBlue;
+}
+</style>
