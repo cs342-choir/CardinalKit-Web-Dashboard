94de7e31b7f2290dbafd3c55d0895c892158ab44
Esteban <julianramos@alternova.co>
2021-05-18 09:27:55 -0500

    Wip (Patients) patients graph and data

--- cardinal-kit-web/src/store/modules/patient/actions.js
+++ cardinal-kit-web/src/store/modules/patient/actions.js
@@ -13,3 +13,41 @@ export const FetchUserHealthData = async ({ commit }, payload) => {
   });
   commit("saveHealthData", records);
 };
+
+export const FetchActivityUser = async ({commit}, payload)=>{
+  let start = new Date('2021-05-12');
+  let userSnap = await request.GET_WHERE(`studies/${payload.studyId}/users/${payload.userId}/healthKit`,
+  [
+    ["header.schema_id.name","==","step-count"],
+    ["create_At",">",start]
+  ]);
+  let records = userSnap.docs.map((record) => {
+    return record.data();
+  });
+  // userSnap = await request.GET_WHERE(`studies/${payload.studyId}/users/${payload.userId}/healthKit`,
+  // [
+  //   ["header.schema_id.name","==","hk-quantity-sample"],
+  //   ["header.create_At",">",start]
+  // ]);
+  // records = records.concat(userSnap.docs.map((record) => {
+  //   return record.data();
+  // }));
+  // userSnap = await request.GET_WHERE(`studies/${payload.studyId}/users/${payload.userId}/healthKit`,
+  // [
+  //   ["header.schema_id.name","==","hk-workout"],
+  //   ["header.create_At",">",start]
+  // ]);
+  // records = records.concat(userSnap.docs.map((record) => {
+  //   return record.data();
+  // }));
+  // userSnap = await request.GET_WHERE(`studies/${payload.studyId}/users/${payload.userId}/healthKit`,
+  // [
+  //   ["header.schema_id.name","==","step-count"],
+  //   ["header.create_At",">",start]
+  // ]);
+  // records = records.concat(userSnap.docs.map((record) => {
+  //   return record.data();
+  // }));
+  console.log("records",records)
+  commit("saveHealthData", records);
+}
