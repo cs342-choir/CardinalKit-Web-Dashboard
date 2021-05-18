ed6a85ed256c8ab3b52ab4130706e1feba546dab
Esteban <julianramos@alternova.co>
2021-05-18 09:27:21 -0500

    Add (firebase )Get where - Firebase rest

--- cardinal-kit-web/src/Rest/firebase/index.js
+++ cardinal-kit-web/src/Rest/firebase/index.js
@@ -10,6 +10,16 @@ export const GET=async (path)=>{
     }
 }
 
+export const GET_WHERE = async (path,whereParams)=>{
+    let ref = db.collection(path)
+    console.log(whereParams)
+    whereParams.forEach(element => {
+        console.log(element)
+        ref=ref.where(element[0],element[1],element[2])
+    });
+    return await ref.get()
+}
+
 export const  POST= async (path,payload)=>{
     if(payload.emptyDoc){
         return await db.collection(path).add(payload.data) 
