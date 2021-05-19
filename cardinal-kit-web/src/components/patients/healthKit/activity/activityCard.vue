278a62b2ca4745934584fd71bda041e3212bfccd
sebastian.peralta <sebastian.peralta@alternova.io>
2021-05-19 18:57:47 -0500

    refactor styles to card

--- cardinal-kit-web/src/components/patients/healthKit/activity/activityCard.vue
+++ cardinal-kit-web/src/components/patients/healthKit/activity/activityCard.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <div class="card-header">
       <h4><b>{{ name }} </b></h4>
-      {{ date }}
+      <div><span>{{ date }}</span></div>
       <!-- Logo -->
     </div>
     <div class="card-body">
@@ -30,12 +30,44 @@ export default {
   computed: {},
 };
 </script>
-<style>
+<style lang="scss">
 .card {
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
   width: 40%;
   border-radius: 5px;
+
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+  }
+  .card-body {
+    .card-info {
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+
+      &__group {
+        text-align: center;
+        padding: 1rem;
+        width: 100%;
+        
+        .subtitle {
+          font-weight: bold;
+          margin-bottom: .8rem;
+          text-transform: capitalize;
+        }
+
+        &:nth-child(n) {
+          border-right: solid lightgray 1px;
+        }
+        &:last-child {
+          border: none;
+        }
+      }
+    }
+  }
 }
 
 .card:hover {
