8fd4daa171976d23beed5fbc94adeceb564aa43c
sebastian.peralta <sebastian.peralta@alternova.io>
2021-05-21 17:42:01 -0500

    created route healthGraphs and redirects

---
+++ cardinal-kit-web/src/views/patients/healthKit/healthKitGraphs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>study: {{studyId}}</p>
+    <p>user: {{userId}}</p>
+    <p>health Kit Workout: {{hkWorkout}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    studyId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    hkWorkout: {
+      type: String,
+      required: true,
+    }
+  },
+  setup() {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
