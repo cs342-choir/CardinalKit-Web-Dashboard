b0062e4aa268273f6cef5f35316252458fafeb23
sebastian.peralta <sebastian.peralta@alternova.io>
2021-05-24 17:58:44 -0500

    logo first version

--- cardinal-kit-web/src/components/auth/Header.vue
+++ cardinal-kit-web/src/components/auth/Header.vue
@@ -1,27 +1,30 @@
 <template>
   <header>
-      <Logo :width="'100'" />
+      <Logo :path="srcLogo"  :width="widthLogo" />
       <div class="header-menu">
-       <router-link class="ev-link" to="#">Profile</router-link>
-       <span class="ev-link" @click="logoutAction">Log out</span>
+       <slot name="menu"></slot>
+       <!-- <span class="ev-link" @click="handleLogout">Log out</span> -->
       </div>
     </header>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import Logo from "@/components/auth/Logo";
 
-export default{
-  methods:{
-    ...mapActions('auth',['logout']),
-    logoutAction(){
-      this.logout().then(()=>{
-        //Action after logout
-      })
-    }
+export default {
+  props: {
+    srcLogo: {
+      type: String
+    },
+    widthLogo: {
+      type: String,
+      default: '50'
+    },
+  },
+  components: {
+      Logo,
   }
 }
-
 </script>
 
 <style lang="scss" scoped>
@@ -33,7 +36,7 @@ header {
   top: 0;
   padding: .5rem 2rem;
   background-color: whitesmoke;
-  box-shadow: 0px 2px 4px 0px #2c2c2c;
+  box-shadow: $shadow-sm;
 
   .header-menu {
     display: flex;
