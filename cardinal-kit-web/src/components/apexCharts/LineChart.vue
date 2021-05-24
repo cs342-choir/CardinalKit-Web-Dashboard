72e473ead51d28c38dfa2a7e9fb4eb984ce67ed2
sebastian.peralta <sebastian.peralta@alternova.io>
2021-05-24 12:46:45 -0500

    added line and bar chart

---
+++ cardinal-kit-web/src/components/apexCharts/LineChart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <apexchart
+      height="300"
+      type="line"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  components: {
+    apexchart: VueApexCharts,
+  },
+  props: {
+    series:{
+      type:Array,
+      required:true
+    }
+  },
+  computed: {
+    chartOptions () {
+      return {
+        chart: {
+          id: "vuechart-line",
+        },
+        xaxis: {
+          type: 'datetime',
+          // labels: {
+          //   format: 'MMM dd HHH'
+          // }
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+      };
+    }
+  },
+};
+</script>
