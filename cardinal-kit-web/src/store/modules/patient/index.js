f8829d8e2336cf14a33b02e6d81a1d76e04d94f6
EstebanRamosFractal <julianramos@alternova.co>
2021-05-27 17:49:45 -0500

    Add fetch all activity Data and graph

--- cardinal-kit-web/src/store/modules/patient/index.js
+++ cardinal-kit-web/src/store/modules/patient/index.js
@@ -1,15 +1,11 @@
 import request from "@/Rest";
 
-import * as activityMutations from './HealthData/Activity/mutations'
-
 export const FetchCategoryTypeData = async (  
   categoryType,
   payload
 ) => {
   let filterParams = [
-    ["body.category_type","==",categoryType],
-    ["header.creation_date_time",">=",new Date('2021-05-21')],
-    ["header.creation_date_time","<=",new Date('2021-05-21')]
+    ["body.category_type","==",categoryType],    
   ]
   return await FetchGeneralData({...payload,filterParams})
 };
@@ -19,12 +15,24 @@ export const FetchQuantityData = async(
   payload
 ) => {
   let filterParams = [
-    ["body.quantity_type","==",quantity_type],
-    ["header.creation_date_time",">=",new Date('2021-05-10')],
-    ["header.creation_date_time","<=",new Date('2021-05-30')]
+    ["body.quantity_type","==",quantity_type]
   ]
   return await FetchGeneralData({...payload,filterParams})
 };
+
+export const FetchActivities = async(payload)=>{
+  let snapShot =  await request
+    .GET(`studies/${payload.studyId}/users/${payload.userId}/healthKit`)
+    .WHERE(["body.activity_name","!=",null])
+    .ORDER_BY("body.activity_name")
+    .ORDER_BY('header.creation_date_time',true)
+    .LIMIT(payload.limit)
+    .Execute()
+    let records = snapShot.docs.map((record) => {
+      return record.data();
+    });
+    return records
+}
 
 const FetchGeneralData = async (payload) => { 
   let Ref = request.GET(`studies/${payload.studyId}/users/${payload.userId}/healthKit`)
@@ -54,14 +62,17 @@ export default {
   state: initialState(),
   mutations:
   {
+    ...require("./HealthData/mutations"),
     ...require("./HealthData/Activity/mutations"),
     ...require("./HealthData/Hearing/mutations"),
   },
   actions: {
+    ...require("./HealthData/actions"),
     ...require("./HealthData/Activity/actions"),
     ...require("./HealthData/Hearing/actions"),
   },
   getters:{
+    ...require("./HealthData/getters"),
     ...require("./HealthData/Activity/getters"),
     ...require("./HealthData/Hearing/getters"),
     
