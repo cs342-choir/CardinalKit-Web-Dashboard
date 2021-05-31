af08c5de9ad569c42a2fe85e0539cb98637fc1f5
EstebanRamosFractal <julianramos@alternova.co>
2021-05-31 11:31:01 -0500

    Add firebase Actions to login with apple Id

--- cardinal-kit-web/src/store/modules/authFirebase/actions.js
+++ cardinal-kit-web/src/store/modules/authFirebase/actions.js
@@ -1,67 +1,93 @@
-import { auth, googleAuthProvider } from '@/plugins/firebase/firebase'
+import {
+  auth,
+  googleAuthProvider,
+  appleAuthProvider,
+} from "@/plugins/firebase/firebase";
 
-export function reset({commit}){
-    commit('RESET')
+export function reset({ commit }) {
+  commit("RESET");
 }
 
-export async function SignIn({commit},payload){
-    console.log("auth",payload)
-    return await 
-      auth.signInWithEmailAndPassword(payload.email, payload.password)
-      .then(() => {
-        commit('isLogged',true)
-        return({
-          'isLogged':true
-        })
-      })
-      .catch((error) => {
-        commit('isLogged',false)
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        return({
-        'isLogged':false,
-        'error':errorMessage,
-        'errorCode':errorCode
-      })
-    });
-
-}
-
-export async function LogInWithGoogle({commit}){
-  return auth.signInWithPopup(googleAuthProvider)
+export async function SignIn({ commit }, payload) {
+  console.log("auth", payload);
+  return await auth
+    .signInWithEmailAndPassword(payload.email, payload.password)
     .then(() => {
-      commit('isLogged',true)
-      return({
-        'isLogged':true
-      })
+      commit("isLogged", true);
+      return {
+        isLogged: true,
+      };
     })
     .catch((error) => {
-      commit('isLogged',false)
+      commit("isLogged", false);
       var errorCode = error.code;
       var errorMessage = error.message;
-      return({
-      'isLogged':false,
-      'error':errorMessage,
-      'errorCode':errorCode
+      return {
+        isLogged: false,
+        error: errorMessage,
+        errorCode: errorCode,
+      };
+    });
+}
+
+export async function LogInWithGoogle({ commit }) {
+  return auth
+    .signInWithPopup(googleAuthProvider)
+    .then(() => {
+      commit("isLogged", true);
+      return {
+        isLogged: true,
+      };
     })
-  })
+    .catch((error) => {
+      commit("isLogged", false);
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return {
+        isLogged: false,
+        error: errorMessage,
+        errorCode: errorCode,
+      };
+    });
 }
 
-export function SignUp({commit},payload){
-  auth.createUserWithEmailAndPassword(payload.email, payload.password)
-  .then((user) => {
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ..
+export async function LogInWithAppleId({ commit }) {
+  return auth
+    .signInWithPopup(appleAuthProvider)
+    .then(() => {
+      commit("isLogged", true);
+      return {
+        isLogged: true,
+      };
+    })
+    .catch((error) => {
+      commit("isLogged", false);
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return {
+        isLogged: false,
+        error: errorMessage,
+        errorCode: errorCode,
+      };
+    });
+}
+
+export function SignUp({ commit }, payload) {
+  auth
+    .createUserWithEmailAndPassword(payload.email, payload.password)
+    .then((user) => {
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+}
+
+export function Logout({ commit }, payload) {
+  auth.signOut().then(() => {
+    console.log("Logout");
+    commit("isLogged", false);
   });
-}
-
-export  function Logout({commit},payload){
-   auth.signOut().then(()=>{
-     console.log("Logout")
-    commit('isLogged',false)
-  })
 }
