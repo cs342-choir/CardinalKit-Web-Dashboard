f833c3a1b48041c64fe6c94d273062f9d626817d
sebastian.peralta <sebastian.peralta@alternova.io>
2021-05-31 20:25:44 -0500

    secod version with hidden popup

--- cardinal-kit-web/src/components/calendar/AltDate.vue
+++ cardinal-kit-web/src/components/calendar/AltDate.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="alt-date">
+  <div ref="divElement" class="alt-date">
 		<label class="alt-datetime-wrapper">
-			<input type="datetime" name="date" />
+			<input autocomplete="off" class="alt-datetime-wrapper__input" placeholder="--/--/--" type="datetime" name="date" v-model="date"/>
 		</label>
-		<div class="alt-popup">
-			<div class="flex alt-calendar">
+		<div v-if="showPopup" class="alt-popup">
+			<div class="alt-calendar">
 				<div class="alt-calendar-group">
 					<label for="days">Day: </label>
 					<select name="days" v-model="today" id="days">
@@ -29,11 +29,14 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed, onUnmounted, ref } from 'vue';
 import { MONTHS } from './const';
 
 export default {
 	setup() {
+		const divElement = ref();
+		const showPopup = ref(false);
+		const date = ref('');
 		const today = ref(new Date().getUTCDate());
 		const currentMonth = ref(new Date().getUTCMonth());
 		const currentYear = ref(new Date().getUTCFullYear());
@@ -52,18 +55,68 @@ export default {
 			return [...Array(countYears).keys()].map((index) => index + initYear);
 		}
 
+		function handleFocus() {
+			showPopup.value = true;
+		}
+
+		function elementIsContain(element) {
+			// console.log(divElement.value?.contains(element.target));
+			showPopup.value = divElement.value?.contains(element.target);
+		}
+
+		window.addEventListener('click', elementIsContain);
+
+		onUnmounted(() => window.removeEventListener('click', elementIsContain));
+
 		return {
 			currentMonth,
 			months,
 			today,
 			days,
 			years,
-			currentYear
+			currentYear,
+			date,
+			handleFocus,
+			showPopup,
+			divElement
 		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
+.alt-date {
+	position: relative;
+	display: inline-flex;
+  flex-direction: column;
+}
+.alt-popup {
+	display: inline-block;
+	padding: 1rem;
+	background: white;
+	border-radius: 3px;
+	margin-top: 15px;
+	position: absolute;
+	top: 37px;
+	z-index: 2;
+}
 
+.alt-calendar {
+	display: flex;
+	gap: 10px;
+}
+.alt-datetime-wrapper {
+	&__input {
+		padding: .5rem;
+		border: solid 2px lightgrey;
+		border-radius: 3px;
+		font-size: 13pt;
+		display: inline-block;
+
+		&::placeholder {
+			color: darkgrey;
+			opacity: 1;
+		}
+	}
+}
 </style>
