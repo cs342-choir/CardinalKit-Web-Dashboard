4c30ad8b54d71564c254d1dc79c77e5a85896a87
EstebanRamosFractal <julianramos@alternova.co>
2021-06-09 09:31:16 -0500

    Add, get studies and users based on user role

--- cardinal-kit-web/src/store/modules/user/actions.js
+++ cardinal-kit-web/src/store/modules/user/actions.js
@@ -10,6 +10,15 @@ export const FetchUserRolesAndStudies= async ({commit})=>{
         auth.onAuthStateChanged(async function (user){
             if(user){
                 const userSnap = await request.GET(`users_roles/${user.uid}`).Execute()
+                if(userSnap.data().rol=="doctor"||userSnap.data().rol=="superAdmin"){
+                    if(userSnap.data().studies.length==1){
+                        let allUsers = [];
+                        const usersSnap = await request.GET(`studies/${userSnap.data().studies[0]}/users`).Execute();
+                        allUsers = usersSnap.docs.map((record) => {
+                          return {id:record.id,...record.data()}
+                        })
+                    }
+                }
                 commit("saveUserRol",{...userSnap.data(),id:user.uid})
                 resolve()
             }
