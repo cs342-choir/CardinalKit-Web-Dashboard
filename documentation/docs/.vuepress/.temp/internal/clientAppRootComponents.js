e00e6e212d3bc8bb985a3ebe7e7f01f76baadbbd
EstebanRamosFractal <julianramos@alternova.co>
2021-09-23 09:22:01 -0500

    Add Documentation Project

---
+++ documentation/docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'D:/Documentos/GitProjects/stanford__cardinalkit_web/docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
